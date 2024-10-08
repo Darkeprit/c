b8c15a93f6d2d0a13cc9e8ad829c40ef5a29734d
Darkeprit <ayaankhairdi238@gmail.com>
2024-10-08 16:59:49 +0530

    Add files via upload

---
+++ areaofsquare.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter size of square :");
+scanf("%d",&a);
+scanf("%d",&b);
+int sum;
+sum=a*b;
+printf("%d",sum);
+return 0;
+}
---
+++ perimeterofrectancle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enetr to length of rectrangle :");
+    scanf("%d",&a);
+     printf("Enetr to breath of rectrangle :");
+    scanf("%d",&b);
+    int peri=2*(a+b);
+    printf("perimeter is : %d",peri);
+}
